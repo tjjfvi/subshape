eb60f02c5b9a9356980f4f98f13d6f34ad801cc8
harrysolovay <harrysolovay@gmail.com>
2022-04-14 08:31:07 -0400

    remove comments from test

--- union.test.ts
+++ union.test.ts
@@ -16,36 +16,6 @@ Deno.test("Unions", () => {
     ]],
   );
 
-  // const c = s.union(
-  //   (value) => {
-  //     return {
-  //       A: 0,
-  //       B: 1,
-  //       C: 2,
-  //       D: 3,
-  //     }[value._tag];
-  //   },
-  //   s.record(["_tag", s.dummy<s.Codec<"A">>("A")]),
-  //   s.record(
-  //     ["_tag", s.dummy<s.Codec<"B">>("B")],
-  //     ["B", s.str],
-  //   ),
-  //   s.record(
-  //     ["_tag", s.dummy<s.Codec<"C">>("C")],
-  //     ["C", s.tuple(s.u32, s.u64)],
-  //   ),
-  //   s.record(
-  //     ["_tag", s.dummy<s.Codec<"D">>("D")],
-  //     [
-  //       "D",
-  //       s.record(
-  //         ["a", s.u32],
-  //         ["b", s.u64],
-  //       ),
-  //     ],
-  //   ),
-  // );
-
   f.visitFixtures(f.fixtures.tagged_union_, (bytes, decoded) => {
     asserts.assertEquals(c.decode(bytes), decoded);
     asserts.assertEquals(c.encode(decoded as any), bytes);
