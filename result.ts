9ca8eaa3d14944241e77db62a31e9cf920425edc
harrysolovay <harrysolovay@gmail.com>
2022-04-14 14:58:25 -0400

    fix design of result according to instance type

--- result.ts
+++ result.ts
@@ -1,73 +1,14 @@
-import { Codec, Native } from "./common.ts";
+import { Codec } from "./common.ts";
+import { Instance } from "./instance.ts";
 import { Union } from "./union.ts";
 
-// TODO: extract `Instance` codec that allows instantiation of a supplied ctor
-
-export interface DataBearer<D> {
-  data: D;
-}
-
-export class Err<
-  ErrDataCodec extends Codec = Codec,
-  Ctor extends new(errData: Native<ErrDataCodec>) => Error & DataBearer<Native<ErrDataCodec>> = new(
-    err: Native<ErrDataCodec>,
-  ) => Error & Native<ErrDataCodec>,
-> extends Codec<InstanceType<Ctor>> {
-  constructor(
-    ctor: Ctor,
-    errDataCodec: ErrDataCodec,
-  ) {
-    super(
-      (value) => {
-        // TODO: confirm the `as` doesn't cause problems elsewhere
-        return errDataCodec._s(value.data);
-      },
-      (cursor, value) => {
-        // TODO: confirm the `as` doesn't cause problems elsewhere
-        return errDataCodec._e(cursor, value.data);
-      },
-      (cursor) => {
-        return new ctor(errDataCodec._d(cursor) as Native<ErrDataCodec>) as InstanceType<Ctor>;
-      },
-    );
-  }
-}
-
-export interface OkBearer<T = any> {
-  ok: T;
-}
-
-export class Ok<
-  ValueCodec extends Codec = Codec,
-  Ctor extends new(ok: Native<ValueCodec>) => OkBearer<Native<ValueCodec>> = new(
-    ok: Native<ValueCodec>,
-  ) => OkBearer<Native<ValueCodec>>,
-> extends Codec<InstanceType<Ctor>> {
-  constructor(
-    ctor: Ctor,
-    valueCodec: ValueCodec,
-  ) {
-    super(
-      (value) => {
-        return valueCodec._s(value.ok);
-      },
-      (cursor, value) => {
-        valueCodec._e(cursor, value.ok);
-      },
-      (cursor) => {
-        return new ctor(valueCodec._d(cursor)) as InstanceType<Ctor>;
-      },
-    );
-  }
-}
-
 export class Result<
-  Ok extends OkBearer,
+  Ok,
   Err extends Error,
 > extends Union<[Ok, Err]> {
   constructor(
     okCodec: Codec<Ok>,
-    errCodec: Codec<Err>,
+    errCodec: Instance<new(...args: any[]) => Err>,
   ) {
     super(
       (value) => {
@@ -78,3 +19,12 @@ export class Result<
     );
   }
 }
+export const result = <
+  Ok,
+  Err extends Error,
+>(
+  okCodec: Codec<Ok>,
+  errCodec: Instance<new(...args: any[]) => any>,
+): Result<Ok, Err> => {
+  return new Result(okCodec, errCodec);
+};
