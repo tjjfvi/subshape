5b2a3975923d3918a3b074f655e098d0a9f7843c
harrysolovay <harrysolovay@gmail.com>
2022-04-22 18:33:26 -0400

    impl key literal union

---
+++ union/key/codec.ts
@@ -0,0 +1,29 @@
+import { Codec } from "../../common.ts";
+import { u8 } from "../../int/codec.ts";
+
+export class KeyLiteralUnion<Member extends PropertyKey> extends Codec<Member> {
+  constructor(...members: Member[]) {
+    const discriminantByKey = members.reduce<Partial<Record<Member, number>>>((acc, cur, i) => {
+      return {
+        ...acc,
+        [cur]: i,
+      };
+    }, {}) as Partial<Record<Member, number>>;
+    super(
+      () => {
+        return u8._s(undefined as any);
+      },
+      (cursor, value) => {
+        const discriminant = discriminantByKey[value]!;
+        u8._e(cursor, discriminant);
+      },
+      (cursor) => {
+        const discriminant = u8._d(cursor);
+        return members[discriminant]!;
+      },
+    );
+  }
+}
+export const keyLiteralUnion = <Member extends PropertyKey>(...members: Member[]): KeyLiteralUnion<Member> => {
+  return new KeyLiteralUnion(...members);
+};
