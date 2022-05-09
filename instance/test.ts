3cd58646ac22fcf469884312cf1af707eeaafd27
tjjfvi <t6@t6.fyi>
2022-05-09 14:33:23 -0400

    improve type safety of instance

--- instance/test.ts
+++ instance/test.ts
@@ -1,20 +1,21 @@
 import * as asserts from "std/testing/asserts.ts";
 import * as s from "../mod.ts";
 
-Deno.test("Instances", () => {
-  class MyError extends Error {
-    constructor(
-      readonly code: number,
-      readonly message: string,
-      readonly payload: {
-        a: string;
-        b: number;
-        c: boolean;
-      },
-    ) {
-      super();
-    }
+class MyError extends Error {
+  constructor(
+    readonly code: number,
+    readonly message: string,
+    readonly payload: {
+      a: string;
+      b: number;
+      c: boolean;
+    },
+  ) {
+    super();
   }
+}
+
+Deno.test("Instances", () => {
   const c = s.instance(
     MyError,
     ["code", s.u8],
@@ -43,3 +44,29 @@ Deno.test("Instances", () => {
   asserts.assertEquals(myErr.message, myErrDecoded.message);
   asserts.assertEquals(myErr.payload, myErrDecoded.payload);
 });
+
+namespace _typeTests {
+  // @ts-ignore: Prevent execution
+  if (1 as 0) return;
+
+  let sPayload = s.record(
+    ["a", s.str],
+    ["b", s.u8],
+    ["c", s.bool],
+  );
+
+  // ok
+  s.instance(MyError, ["code", s.u8], ["message", s.str], ["payload", sPayload]);
+
+  // @ts-expect-error: Missing constructor parameters
+  s.instance(MyError);
+
+  // @ts-expect-error: Constructor parameter type mismatch
+  s.instance(MyError, ["code", s.u8], ["message", s.str], ["name", s.str]);
+
+  // @ts-expect-error: Missing field
+  s.instance(MyError, ["code", s.u8], ["message", s.str], ["paidload", sPayload]);
+
+  // @ts-expect-error: Field type mismatch
+  s.instance(MyError, ["code", s.u8], ["message", s.str], ["name", sPayload]);
+}
