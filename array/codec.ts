0af3e245d3bf3c4fafc849d5da41d3cf79b16543
tjjfvi <t6@t6.fyi>
2022-05-15 12:34:33 -0400

    remove unneccesary destructuring from array codec

--- array/codec.ts
+++ array/codec.ts
@@ -2,25 +2,23 @@ import { Codec, createCodec } from "../common.ts";
 import { compact } from "../compact/codec.ts";
 
 export function array<T>($el: Codec<T>) {
-  const { _encode: compact_encode, _decode: compact_decode } = compact;
-  const { _encode, _decode } = $el;
   return createCodec<T[]>({
     _staticSize: 4,
     _encode(buffer, value) {
-      compact_encode(buffer, value.length);
+      compact._encode(buffer, value.length);
       if (value.length) {
         buffer.push(value.length * $el._staticSize);
         for (let i = 0; i < value.length; i++) {
-          _encode(buffer, value[i]!);
+          $el._encode(buffer, value[i]!);
         }
         buffer.pop();
       }
     },
     _decode(buffer) {
-      const length = Number(compact_decode(buffer));
+      const length = Number(compact._decode(buffer));
       const value: T[] = Array(length);
       for (let i = 0; i < value.length; i++) {
-        value[i] = _decode(buffer);
+        value[i] = $el._decode(buffer);
       }
       return value;
     },
