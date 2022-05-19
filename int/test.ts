0baf3bcd07737ba88a0dc8f5388d875e1a98d930
T6 <t6@t6.fyi>
2022-05-19 07:07:52 -0400

    Add 256 bit ints (#47)
    * add u256 and i256
    
    * add primitive-types to Cargo.toml
    
    * rework int tests

--- int/test.ts
+++ int/test.ts
@@ -1,14 +1,37 @@
 import * as $ from "../mod.ts";
 import { testCodec } from "../test-util.ts";
 
-testCodec("u8", $.u8, [0, 1, 2, 9]);
-testCodec("u16", $.u16, [0, 1, 2, 9]);
-testCodec("u32", $.u32, [0, 1, 2, 9]);
-testCodec("u64", $.u64, [0n, 1n, 2n, 9n]);
-testCodec("u128", $.u128, [0n, 1n, 2n, 9n]);
+function generateIntTests(signed: boolean, size: number) {
+  const s = BigInt(size);
+  return signed
+    ? [
+      -1n * 2n ** (s - 1n), // min value
+      -1n * 2n ** (s / 2n - 1n), // min value of last size
+      -1n,
+      0n,
+      1n,
+      2n ** (s / 2n - 1n) - 1n, // max value of last size
+      2n ** (s - 1n) - 1n, // max value
+    ]
+    : [
+      0n,
+      1n,
+      2n ** (s / 2n) - 1n, // max value of last size
+      2n ** s - 1n, // max value
+    ];
+}
 
-testCodec("i8", $.i8, [-9, -2, -1, 0, 1, 2, 9]);
-testCodec("i16", $.i16, [-9, -2, -1, 0, 1, 2, 9]);
-testCodec("i32", $.i32, [-9, -2, -1, 0, 1, 2, 9]);
-testCodec("i64", $.i64, [-9n, -2n, -1n, 0n, 1n, 2n, 9n]);
-testCodec("i128", $.i128, [-9n, -2n, -1n, 0n, 1n, 2n, 9n]);
+testCodec("u8", $.u8, generateIntTests(false, 8).map(Number));
+testCodec("u16", $.u16, generateIntTests(false, 16).map(Number));
+testCodec("u32", $.u32, generateIntTests(false, 32).map(Number));
+testCodec("u64", $.u64, generateIntTests(false, 64).map(BigInt));
+testCodec("u128", $.u128, generateIntTests(false, 128).map(BigInt));
+testCodec("u256", $.u256, generateIntTests(false, 256).map(BigInt));
+
+testCodec("i8", $.i8, generateIntTests(true, 8).map(Number));
+testCodec("i16", $.i16, generateIntTests(true, 16).map(Number));
+testCodec("i32", $.i32, generateIntTests(true, 32).map(Number));
+testCodec("i64", $.i64, generateIntTests(true, 64).map(BigInt));
+testCodec("i128", $.i128, generateIntTests(true, 128).map(BigInt));
+testCodec("i128 (rust: [i64; 2])", $.i128, generateIntTests(true, 128).map(BigInt));
+testCodec("i256 (rust: [i128; 2])", $.i256, generateIntTests(true, 256).map(BigInt));
