a4cd290420fa24ababf372bd8a28deaad2402ba7
tjjfvi <t6@t6.fyi>
2022-06-08 17:03:46 -0400

    add $.transform

---
+++ transform/test.ts
@@ -0,0 +1,6 @@
+import * as $ from "../mod.ts";
+import { testCodec } from "../test-util.ts";
+
+const $boxU8 = $.transform($.u8, ({ value }: { value: number }) => value, (value) => ({ value }));
+
+testCodec("boxU8", $boxU8, [{ value: 0 }, { value: 1 }]);
