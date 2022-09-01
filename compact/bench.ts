ca62e9dedeb218baf591bfd4bc4475a12739d0d5
tjjfvi <t6@t6.fyi>
2022-09-01 09:09:01 -0400

    fix benchmarks

--- compact/bench.ts
+++ compact/bench.ts
@@ -1,8 +1,15 @@
 import { benchCodec } from "../test-util.ts";
 import * as $ from "./codec.ts";
 
-benchCodec("compact (u8)", $.compact, 2 ** (8 - 2) - 1);
-benchCodec("compact (u16)", $.compact, 2 ** (16 - 2) - 1);
-benchCodec("compact (u32)", $.compact, 2 ** (32 - 2) - 1);
-benchCodec("compact (b64)", $.compact, 2n ** 64n - 1n);
-benchCodec("compact (b128)", $.compact, 2n ** 128n - 1n);
+benchCodec("compactU32 (6)", $.compactU32, 2 ** 6 - 1);
+benchCodec("compactU32 (14)", $.compactU32, 2 ** 14 - 1);
+benchCodec("compactU32 (30)", $.compactU32, 2 ** 30 - 1);
+benchCodec("compactU32 (32)", $.compactU32, 2 ** 32 - 1);
+
+benchCodec("compactU256 (6)", $.compactU256, 2n ** 6n - 1n);
+benchCodec("compactU256 (14)", $.compactU256, 2n ** 14n - 1n);
+benchCodec("compactU256 (30)", $.compactU256, 2n ** 30n - 1n);
+benchCodec("compactU256 (32)", $.compactU256, 2n ** 32n - 1n);
+benchCodec("compactU256 (64)", $.compactU256, 2n ** 64n - 1n);
+benchCodec("compactU256 (128)", $.compactU256, 2n ** 128n - 1n);
+benchCodec("compactU256 (256)", $.compactU256, 2n ** 256n - 1n);
