c6771cfa009090d42bf34bc1c613c44aba345684
tjjfvi <t6@t6.fyi>
2022-09-16 08:30:40 -0700

    allow Codec<never> in codec combinators

--- object/codec.ts
+++ object/codec.ts
@@ -1,28 +1,28 @@
-import { Codec, createCodec, Expand, Narrow, Native, U2I } from "../common.ts";
+import { AnyCodec, Codec, createCodec, Expand, Narrow, Native, U2I } from "../common.ts";
 
-export type Field<K extends keyof any = keyof any, V = any> = [key: K, value: Codec<V>];
+export type AnyField = [key: keyof any, value: AnyCodec];
 
-export type NativeField<F extends Field> = { [_ in F[0]]: Native<F[1]> };
+export type NativeField<F extends AnyField> = { [_ in F[0]]: Native<F[1]> };
 
 // // TODO: do we prefer the following (variadic) approach?
-export type NativeObject<O extends Field[]> = Expand<
+export type NativeObject<O extends AnyField[]> = Expand<
   U2I<
     | {}
     | {
-      [K in keyof O]: O[K] extends Field<infer K, infer V> ? Record<K, V> : never;
+      [K in keyof O]: Record<O[K][0], Native<O[K][1]>>;
     }[number]
   >
 >;
 
-export function object<O extends Field[]>(...fields: Narrow<O>): Codec<NativeObject<O>>;
-export function object<O extends Field[]>(...fields: O): Codec<NativeObject<O>> {
+export function object<O extends AnyField[]>(...fields: Narrow<O>): Codec<NativeObject<O>>;
+export function object<O extends AnyField[]>(...fields: O): Codec<NativeObject<O>> {
   return createCodec({
     name: "object",
     _metadata: [object, ...fields] as any,
     _staticSize: fields.map((x) => x[1]._staticSize).reduce((a, b) => a + b, 0),
     _encode(buffer, value) {
       fields.forEach(([key, fieldEncoder]) => {
-        fieldEncoder._encode(buffer, (value as any)[key]);
+        fieldEncoder._encode(buffer, (value as any)[key] as never);
       });
     },
     _decode(buffer) {
