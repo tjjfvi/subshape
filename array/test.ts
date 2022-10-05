253200808c4c650394d18f2f6187199ec9e77d5c
tjjfvi <t6@t6.fyi>
2022-10-04 19:36:23 -0700

    fix casing of uint8Array and sizedUint8Array

--- array/test.ts
+++ array/test.ts
@@ -12,14 +12,14 @@ testCodec("[u8; 1]", $.sizedArray($.u8, 1), [[1]]);
 testCodec("[u8; 2]", $.sizedArray($.u8, 2), [[1, 1]]);
 testCodec("[u8; 100]", $.sizedArray($.u8, 100), [Array(100).fill(1) as any]);
 
-testCodec("uint8array", $.uint8array, [
+testCodec("uint8Array", $.uint8Array, [
   new Uint8Array([1, 2, 3, 4, 5]),
   new Uint8Array([6, 7, 8, 9, 10]),
   new Uint8Array([11, 12, 13, 14, 15]),
   new Uint8Array([16, 17, 18, 19, 20, 21]),
 ]);
 
-testCodec("sizedUint8array", $.sizedUint8array(4), [
+testCodec("sizedUint8Array", $.sizedUint8Array(4), [
   new Uint8Array([0, 0, 0, 0]),
   new Uint8Array([1, 2, 3, 4]),
 ]);
