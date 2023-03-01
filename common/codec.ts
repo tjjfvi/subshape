87be16daa91ba2e2bfdeba98e074f3775ec9ee05
tjjfvi <t6@t6.fyi>
2023-03-01 09:30:07 -0500

    fix AnyCodec

--- common/codec.ts
+++ common/codec.ts
@@ -3,7 +3,6 @@ import { DecodeBuffer, EncodeBuffer } from "./buffer.ts"
 import { Metadata } from "./metadata.ts"
 import { ScaleAssertError, ScaleEncodeError } from "./util.ts"
 
-export type AnyCodec = Codec<any> | Codec<never>
 export type Native<T extends AnyCodec> = T extends Codec<infer U> ? U : never
 
 export function createCodec<T>(
@@ -39,7 +38,64 @@ const codecInspectCtx = new Map<Codec<any>, number | null>()
 let codecInspectIdN = 0
 const nodeCustomInspect = Symbol.for("nodejs.util.inspect.custom")
 const denoCustomInspect = Symbol.for("Deno.customInspect")
-export abstract class Codec<T> {
+
+abstract class _Codec {
+  private [nodeCustomInspect](_0: unknown, _1: unknown, inspect: (value: unknown) => string) {
+    return this._inspect(inspect)
+  }
+
+  private [denoCustomInspect](inspect: (value: unknown, opts: unknown) => string, opts: unknown) {
+    return this._inspect((x) => inspect(x, opts))
+  }
+
+  // Properly handles circular codecs in the case of $.deferred
+  private _inspect(inspect: (value: unknown) => string): string
+  private _inspect<T>(this: Codec<T>, inspect: (value: unknown) => string): string {
+    let id = codecInspectCtx.get(this)
+    if (id !== undefined) {
+      if (id === null) {
+        codecInspectCtx.set(this, id = codecInspectIdN++)
+      }
+      return `$${id}`
+    }
+    try {
+      codecInspectCtx.set(this, null)
+      let content = ""
+      for (const metadata of this._metadata) {
+        if (metadata.type === "docs") {
+          // TODO: print docs in inspect
+        } else {
+          if (metadata.type === "atomic") {
+            content += metadata.name
+          } else if (metadata.type === "factory") {
+            content += `${metadata.name}(${inspect(metadata.args).replace(/^\[(?: (.+) |(.+))\]$/s, "$1$2")})`
+          }
+          break
+        }
+      }
+      content ||= "?"
+      id = codecInspectCtx.get(this)
+      return id !== null ? `$${id} = ${content}` : content
+    } finally {
+      codecInspectCtx.delete(this)
+      if (codecInspectCtx.size === 0) codecInspectIdN = 0
+    }
+  }
+}
+
+export interface AnyCodec extends _Codec {
+  _staticSize: number
+  _encode(buffer: EncodeBuffer, value: any): void
+  _decode: (buffer: DecodeBuffer) => any
+  _assert: (state: AssertState) => void
+  _metadata: any
+
+  encode(value: any): Uint8Array
+  encodeAsync(value: any): Promise<Uint8Array>
+  decode(array: Uint8Array): any
+}
+
+export abstract class Codec<in out T> extends _Codec implements AnyCodec {
   /** A static estimation of the size, which may be an under- or over-estimate */
   abstract _staticSize: number
   /** Encodes the value into the supplied buffer, which should have at least `_staticSize` free byte. */
@@ -70,48 +126,6 @@ export abstract class Codec<T> {
   decode(array: Uint8Array) {
     const buf = new DecodeBuffer(array)
     return this._decode(buf)
-  }
-
-  private [nodeCustomInspect](_0: unknown, _1: unknown, inspect: (value: unknown) => string) {
-    return this._inspect(inspect)
-  }
-
-  private [denoCustomInspect](inspect: (value: unknown, opts: unknown) => string, opts: unknown) {
-    return this._inspect((x) => inspect(x, opts))
-  }
-
-  // Properly handles circular codecs in the case of $.deferred
-  private _inspect(inspect: (value: unknown) => string): string
-  private _inspect(this: Codec<T>, inspect: (value: unknown) => string): string {
-    let id = codecInspectCtx.get(this)
-    if (id !== undefined) {
-      if (id === null) {
-        codecInspectCtx.set(this, id = codecInspectIdN++)
-      }
-      return `$${id}`
-    }
-    try {
-      codecInspectCtx.set(this, null)
-      let content = ""
-      for (const metadata of this._metadata) {
-        if (metadata.type === "docs") {
-          // TODO: print docs in inspect
-        } else {
-          if (metadata.type === "atomic") {
-            content += metadata.name
-          } else if (metadata.type === "factory") {
-            content += `${metadata.name}(${inspect(metadata.args).replace(/^\[(?: (.+) |(.+))\]$/s, "$1$2")})`
-          }
-          break
-        }
-      }
-      content ||= "?"
-      id = codecInspectCtx.get(this)
-      return id !== null ? `$${id} = ${content}` : content
-    } finally {
-      codecInspectCtx.delete(this)
-      if (codecInspectCtx.size === 0) codecInspectIdN = 0
-    }
   }
 }
 
