f651ff0e0f3e894d81f59ac5a2060980f118d816
tjjfvi <t6@t6.fyi>
2023-06-29 10:58:51 +0200

    fmt

--- common/mod.ts
+++ common/mod.ts
@@ -1,7 +1,7 @@
 // moderate
 
-export * from "./assert.ts";
-export * from "./buffer.ts";
-export * from "./codec.ts";
-export * from "./metadata.ts";
-export * from "./util.ts";
+export * from "./assert.ts"
+export * from "./buffer.ts"
+export * from "./codec.ts"
+export * from "./metadata.ts"
+export * from "./util.ts"
