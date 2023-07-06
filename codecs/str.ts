ee7b8eaded1a4ed0e507688334cbe2c501ede717
T6 <t6@t6.fyi>
2023-07-06 15:23:22 +0200

    perf: improve that of `$.str` (#167)

--- codecs/str.ts
+++ codecs/str.ts
@@ -4,11 +4,13 @@ import { u32 } from "./int.ts"
 
 const compactU32 = compact(u32)
 
+const textEncoder = new TextEncoder()
+const textDecoder = new TextDecoder()
 export const str: Codec<string> = createCodec({
   _metadata: metadata("$.str"),
   _staticSize: compactU32._staticSize,
   _encode(buffer, value) {
-    const array = new TextEncoder().encode(value)
+    const array = textEncoder.encode(value)
     compactU32._encode(buffer, array.length)
     buffer.insertArray(array)
   },
@@ -17,9 +19,9 @@ export const str: Codec<string> = createCodec({
     if (buffer.array.length < buffer.index + len) {
       throw new ScaleDecodeError(this, buffer, "Attempting to `str`-decode beyond bounds of input bytes")
     }
-    const slice = buffer.array.slice(buffer.index, buffer.index + len)
+    const slice = buffer.array.subarray(buffer.index, buffer.index + len)
     buffer.index += len
-    return new TextDecoder().decode(slice)
+    return textDecoder.decode(slice)
   },
   _assert(assert) {
     assert.typeof(this, "string")
